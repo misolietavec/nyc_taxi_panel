5d8b86b19bfbdef7829c476d0cabf4a69b47e3a5
Michal Kaukič <mike@feelmath.eu>
2024-01-29 14:05:08 +0100

    pridany datashader, obrazky predrobene

--- taxi_panel.py
+++ taxi_panel.py
@@ -7,23 +7,19 @@ import ipywidgets as ipw
 from bokeh.io import curdoc as document # len kvoli nazvu v prehliadaci
 from data_functions_sk import make_graphs, static_graphs
 from ipyleaflet import Map, MarkerCluster, CircleMarker
+from PIL import Image
 pn.config.throttled = True
 pn.extension('plotly','ipywidgets')
 
-
 df = pl.read_parquet('data/nyc_taxi155k.parq')
 meanloc = [df['pick_lat'].mean(), df['pick_lon'].mean()]
-
 
 pick_days, drop_days, pick_hours, drop_hours = static_graphs(df)
 static_days = pn.Column(pick_days, drop_days)
 static_hours = pn.Column(pick_hours, drop_hours)
 
-
 dfdays = make_graphs(df, create=False)  # grafy predrobene uz pre tento vyber
 
-
-# vyber dna, hodiny, smeru (nastup, vystup)
 day_choose = pnw.IntSlider(start=1, end=31, value=14, width=250, name='Deň')
 hour_choose = pnw.IntSlider(start=0, end=23, value=11, width=300, name='Hodina')
 day_player = pnw.DiscretePlayer(options=list(range(1, 32)), interval=300, value=1, show_loop_controls=False)
@@ -40,8 +36,10 @@ def play_hourly(value, dnuvon):
     graf.update_yaxes(range=[0, 750])  # odhad z grafov
     return graf
     
+
 bind_hourly = pn.bind(play_hourly, value=day_player, dnuvon=smer)
 play_col = pn.Column(day_player, smer, bind_hourly)
+
 
 @pn.depends(day_choose, smer)
 def view_hourly(day_choose, smer):
@@ -58,7 +56,7 @@ def view_totals(day_or_hour):
 mapa =  Map(center=meanloc, layout=ipw.Layout(width='750px', height='450px'))
 bod =  CircleMarker(location=meanloc, radius=6, visible=False)
 body = MarkerCluster(markers=[bod] * 10, visible=False)
-mapa.add(body);
+mapa.add(body)
 
 
 @pn.depends(day_choose, hour_choose, smer)
@@ -106,8 +104,23 @@ def view_rtimes(nbins):
     return px.bar(data_frame=df_hist, x='x', y='y', 
                   barmode='group', labels={'x': 'Čas jazdy (min.)', 'y': 'početnosť',
                                            'variable': 'hodnota'}, width=900, height=350)
-
 dist_and_times = pn.Column(pn.Spacer(height=20), nbins, view_distances, view_rtimes)
+
+
+pick_images, drop_images = {}, {}
+for hour in range(24):
+    pick_images[hour] = Image.open(f'images/pick_{hour:02d}.jpg')
+    drop_images[hour] = Image.open(f'images/drop_{hour:02d}.jpg')
+
+
+@pn.depends(hour_choose, smer)
+def view_shaded(hour_choose, smer):
+    images = pick_images if (smer == 'Nástup') else drop_images
+    return images[hour_choose]
+shaded_md = pn.pane.Markdown("### Všetkých 11 mil. zápisov, spracované cez datashader")
+shaded = pn.Column(pn.Spacer(height=20), shaded_md, pn.Row(smer, hour_choose), view_shaded)
+
+
 nadpis = pn.pane.Markdown(
     f"""
     # Taxi v New Yorku
@@ -119,6 +132,6 @@ maps = pn.Column( pn.Spacer(height=20), pn.Row(smer, day_choose, hour_choose), r
 totals = pn.Column(pn.Spacer(height=20), day_or_hour, view_totals)
 
 tabs = pn.Tabs(('Grafy podľa dní', hourly), ('Grafy celkové', totals),
-               ('Miesta na mape', maps), ('Vzdialenosti', dist_and_times),
-               ('Prehrávač', play_col), dynamic=True)
+               ('Miesta na mape', maps), ('Histogramy', dist_and_times),
+               ('Prehrávač', play_col), ('Datashader', shaded), dynamic=True)
 pn.Column(nadpis, pn.Spacer(height=25), tabs).servable()
